6c622c05c2bc7435959b2b4e555b1259b39f0e9a
Susan Potter <me@susanpotter.net>
2011-02-15 00:21:14 -0600

    Fix finapp supervisor's module name
    Module name of finapp's supervisor template uses {{name}} now instead of
    hard coded "tint". My bad.

--- finapp_sup.erl
+++ finapp_sup.erl
@@ -4,15 +4,17 @@
 %%% @copyright {{copyright_year}} {{author_name}}
 %%% @doc {{description}}
 %%% @end
--module(tint_sup).
+-module({{name}}_sup).
 
 -behaviour(supervisor).
 -export([start_link/0]).
 -export([init/1]).
+
+-define(SERVER, ?MODULE).
 %%%.
 %%%'   PUBLIC API
 start_link() ->
-  supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+  supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
 %%%.
 %%%'   CALLBACKS
